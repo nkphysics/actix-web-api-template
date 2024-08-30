dd4b6f3d7fb84b181686c7ed41109e26c8ff4d34
Tsar Bomba Nick <njkuechel@protonmail.com>
2024-08-29 21:44:32 -0600

    Created get and post functionality for a to-do list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
+use std::sync::Mutex;
 
+
+struct Appstate {
+    entries: Mutex<Vec<ListEntry>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct ListEntry {
+    id: i32,
+    entry: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct InsertEntrydata {
+    pub entry: String,
+}
+
+#[derive(Deserialize, Clone)]
+struct UpdateEntrydata {
+    pub entry: String,
+}
 
 #[derive(Serialize)]
 struct Status {
     pub status: String,
+}
+
+#[get("/list/entries")]
+async fn get_list(data: web::Data<Appstate>) -> impl Responder {
+    HttpResponse::Ok().json(data.entries.lock().unwrap().to_vec())
+}
+
+#[post("/list/entries")]
+async fn insert_entry(data: web::Data<Appstate>,
+                      entry_info: web::Json<InsertEntrydata>) -> impl Responder {
+    let mut entries = data.entries.lock().unwrap();
+    let mut maxid: i32 = 0;
+    for i in 0..entries.len() {
+        if entries[i].id > maxid{
+            maxid = entries[i].id;
+        }
+    }
+    entries.push(ListEntry{
+        id: maxid + 1,
+        entry: entry_info.entry.clone(),
+    });
+
+    HttpResponse::Ok().json(entries.to_vec())
 }
 
 #[get("/status")]
@@ -30,12 +74,16 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_data = web::Data::new(Appstate {entries: Mutex::new(vec![])});
+    HttpServer::new(move || {
         App::new()
+            .app_data(app_data.clone())
             .service(hello)
             .service(echo)
-            .service(status)
             .route("/hey", web::get().to(manual_hello))
+            .service(status)
+            .service(get_list)
+            .service(insert_entry)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
